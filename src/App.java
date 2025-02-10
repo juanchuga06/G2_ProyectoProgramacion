899a6f6d15ee044490ba40b075ecd88e06d1ae91
JuanCa <juan.chuga@epn.edu.ec>
2025-02-10 09:11:50 -0500

    Hot fix

--- src/App.java
+++ src/App.java
@@ -1,13 +1,10 @@
 
 import javax.swing.SwingUtilities;
 
-import UserInterfaceComponent.Form.LoginFrame;
 import UserInterfaceComponent.Form.MainForm;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
-        // SwingUtilities.invokeLater(LoginFrame::new);
 
         SwingUtilities.invokeLater(MainForm::new);
 
